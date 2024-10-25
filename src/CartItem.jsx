38dfaf3fa915949e47031bbaa1e9ba6dbe24dad1
Xahep <xahepjulian45@gmail.com>
2024-10-25 08:22:56 -0500

    Finish project & features

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -10,7 +10,7 @@ const CartItem = ({ onContinueShopping }) => {
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
     let totalCost = 0;
-    
+
     cart.forEach(element => {
       const costNumber = parseFloat(element.cost.replace('$', ''));
       totalCost += element.quantity * costNumber;
@@ -22,28 +22,29 @@ const CartItem = ({ onContinueShopping }) => {
     onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
     const isQuantityZero = item.quantity - 1;
     if (isQuantityZero !== 0) {
-      dispatch(updateQuantity({name: item.name, quantity: isQuantityZero}));
+      dispatch(updateQuantity({ name: item.name, quantity: isQuantityZero }));
     } else {
-      dispatch(removeItem({name: item.name}));
+      dispatch(removeItem({ name: item.name }));
     }
   };
 
   const handleRemove = (item) => {
-    dispatch(removeItem({name: item.name}));
+    dispatch(removeItem({ name: item.name }));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
-    console.log(item);
     const costNumber = parseFloat(item.cost.replace('$', ''));
     return costNumber * item.quantity;
   };
@@ -73,7 +74,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) =>handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
